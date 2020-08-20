137760077aa3140384f9155557c301123d0ab7ba
CodyClark07 <codyclark2010@hotmail.com>
2020-08-20 10:02:53 -0600

    follow/unfollow persists on refresh

--- clientsrc/src/store/Dashboard.js
+++ clientsrc/src/store/Dashboard.js
@@ -49,25 +49,6 @@ export default {
       let res = await api.get('profile/' + id + '/followers')
       commit("setFollowers", res.data.data)
     },
-    async searchDashboard({ commit, dispatch }, query) {
-
-      let profile = await api.get("profile/name" + query)
-      // console.log(profile.data[0].id);
-      if (profile.data.length > 0) {
-
-        // commit("setFriendDashboard", profile.data[0])
-
-        router.push({ name: 'friendDashboard', params: { _id: profile.data[0].id } })
-      }
-      else {
-        router.push({ name: 'Games' })
-
-        let newQuery = query.toLowerCase().replace(/ /g, '-');
-        let game = await gameApi.get('games?search=' + newQuery)
-        commit("setSearchedGames", game.data.results)
-
-      }
-    },
     async getDashboard({ commit, dispatch }, id) {
 
       let res = await api.get("profile/user/" + id)
